752ac352848ba2f0e96cc5db033b11b2450cac8f
monusagar247 <monusagar247@gmail.com>
2025-10-17 12:38:32 +0530

    added instagram

--- server.js
+++ server.js
@@ -35,6 +35,17 @@ app.use(limiter);
 app.get('/', (req, res) => res.status(200).json({ message: 'Server running cleanly ⚡' }));
 
 
+app.get("/api/instagram", async (req, res) => {
+  const { url } = req.query;
+  try {
+    const response = await fetch(`https://nayan-video-downloader.vercel.app/instagram?url=${encodeURIComponent(url)}`);
+    const data = await response.json();
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to fetch data" });
+  }
+});
+
 app.get("/api/download", async (req, res) => {
   const { url } = req.query;
   try {
